6f364322eee7f45644eac9a9a4552065a740f870
juan martin castro <juanmartincastro.13@gmail.com>
2022-08-09 20:52:25 -0300

    Desafio Express

---
+++ 3eraEntrega/3erDesafio.js
@@ -0,0 +1,23 @@
+const { request, response } = require("express");
+const express = require("express");
+const Contenedor = require('../2daEntrega/2daEntrega')
+const arrayDeProductos = new Contenedor();
+
+const app = express();
+
+
+app.get("/productos", (req, res) => {
+    res.send(arrayDeProductos.getAll());
+})
+
+app.get("/productoRandom", (req, res) => {
+    response.send('Devuelve un producto al azar');
+})
+
+app.get("*", (req, res) => {
+    response.send('Error 404 - Page not found');
+})
+
+const server = app.listen(8080, () => {
+    console.log(`servidor http escuchando en http://localhost:8080/`)
+} )
